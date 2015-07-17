7e19926b6986a9d6d0c54fe346971ac89cd6a14b
afornie <antonio@splendo.com>
2015-07-17 11:13:30 +0200

    Factor out to scripts

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" == "rc" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-  appengine-java-sdk-1.9.23/bin/appcfg.sh update appengine-modules-ear/build/exploded-app
-  echo "In rc branch or PR"
-  echo "${TRAVIS_BRANCH}"
-  echo "${TRAVIS_PULL_REQUEST}"
-else
-  echo "Not in rc branch or PR"
-  echo "${TRAVIS_BRANCH}"
-  echo "${TRAVIS_PULL_REQUEST}"
-fi;
+appengine-java-sdk-1.9.23/bin/appcfg.sh update appengine-modules-ear/build/exploded-app
