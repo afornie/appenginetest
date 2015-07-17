b6f02ba220aa8a551afe383db448e6268b0c56ab
afornie <antonio@splendo.com>
2015-07-17 07:52:20 +0200

    Again && syntax

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" == "rc" ] -a [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+if [ "${TRAVIS_BRANCH}" == "rc" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   appengine-java-sdk-1.9.23/bin/appcfg.sh update appengine-modules-ear/build/exploded-app
   echo "In rc branch or PR"
   echo "${TRAVIS_BRANCH}"
