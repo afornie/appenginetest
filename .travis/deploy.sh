f68a26af29e2fe2f1c773c59c726d74fd443308b
afornie <antonio@splendo.com>
2015-07-17 07:45:24 +0200

    And syntax with && instead of -a

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" == "rc" ] -a [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+if [ "${TRAVIS_BRANCH}" == "rc" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   appengine-java-sdk-1.9.23/bin/appcfg.sh update appengine-modules-ear/build/exploded-app
-  echo "In rc branch or PR"
+  echo "In rc branch and NOT PR"
   echo "${TRAVIS_BRANCH}"
   echo "${TRAVIS_PULL_REQUEST}"
 else
