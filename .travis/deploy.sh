10fae8eec666cc94c7ed652ce23efb53f2a8890c
afornie <antonio@splendo.com>
2015-07-17 11:23:26 +0200

    Improve syntax

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Preparation - assumes app alraedy exploded
+openssl aes-256-cbc -K $encrypted_a6c05f649a4b_key -iv $encrypted_a6c05f649a4b_iv -in .appcfg_oauth2_tokens_java.enc -out .appcfg_oauth2_tokens_java -d
+cp .appcfg_oauth2_tokens_java ~
+wget https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.23.zip
+unzip -q appengine-java-sdk-1.9.23.zip
+
+# Effective deployment
 appengine-java-sdk-1.9.23/bin/appcfg.sh update appengine-modules-ear/build/exploded-app
--- .travis/prepare_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-openssl aes-256-cbc -K $encrypted_a6c05f649a4b_key -iv $encrypted_a6c05f649a4b_iv -in .appcfg_oauth2_tokens_java.enc -out .appcfg_oauth2_tokens_java -d
-cp .appcfg_oauth2_tokens_java ~
-wget https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.23.zip
-unzip -q appengine-java-sdk-1.9.23.zip
